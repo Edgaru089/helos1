db6959a932ab615e0bdecc37ee14fa79d22a4c1b
Edgaru089 <sjs333@outlook.com>
2021-11-14 17:30:33 +0800

    makefile: run QEMU with reboot/shutdown disabled

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 
-qemu-system-x86_64 -bios ../OVMF.fd -drive file=fat:rw:../FAT,format=raw,media=disk -m 2G -serial stdio
+qemu-system-x86_64 -no-reboot -no-shutdown -d cpu_reset -bios ../OVMF.fd -drive file=fat:rw:../FAT,format=raw,media=disk -m 2G -serial stdio
 
 
