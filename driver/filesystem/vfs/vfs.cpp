c405aa04f503d60913e242f3a559a749fb84aa90
Edgaru089 <sjs333@outlook.com>
2021-11-23 01:57:43 +0800

    driver/filesystem: VFS (WIP. 2)

--- driver/filesystem/vfs/vfs.cpp
+++ driver/filesystem/vfs/vfs.cpp
@@ -1,50 +1,47 @@
 
 #include "vfs.hpp"
 #include "string.h"
+#include "../../../interrupt/interrupt.h"
 
 namespace helos {
 namespace filesystem {
 
 
-struct __VirtualFilesystem_Mount {
-	char       *where;
-	size_t      len;
-	Filesystem *fs;
-};
+VirtualFilesystem *VFS;
 
-VirtualFilesystem::VirtualFilesystem(): root(nullptr) {
-	mounts = vector_Create(sizeof(__VirtualFilesystem_Mount));
+VirtualFilesystem::VirtualFilesystem() {
+	mounts = vector_Create(sizeof(void *));
 }
 
 VirtualFilesystem::~VirtualFilesystem() {
 	for (int i = 0; i < vector_Size(mounts); i++) {
-		delete ((__VirtualFilesystem_Mount *)vector_At(mounts, i))->fs;
-		delete[]((__VirtualFilesystem_Mount *)vector_At(mounts, i))->where;
+		delete (*((__VirtualFilesystem_Mount **)vector_At(mounts, i)))->fs;
+		delete *((__VirtualFilesystem_Mount **)vector_At(mounts, i));
 	}
-	if (root)
-		delete root;
+	if (root.fs)
+		delete root.fs;
 	vector_Destroy(mounts);
 }
 
-int VirtualFilesystem::Mount(const char *where, Filesystem *fs) {
+int VirtualFilesystem::Mount(const char *where, const char *source, Filesystem *fs) {
 	size_t len = strlen(where);
 	while (len > 1 && where[len - 1] == Seperator) // ditch the trailing seperators
 		len--;
 
-	__VirtualFilesystem_Mount m;
-	m.fs    = fs;
-	m.len   = len;
-	m.where = new char[len + 1];
-	memcpy(m.where, where, len);
-	m.where[len] = 0;
+	__VirtualFilesystem_Mount *m = new __VirtualFilesystem_Mount;
+	m->where                     = runtime::String(where, len);
+	m->source                    = source;
 
+	INTERRUPT_DISABLE;
 	for (int i = 0; i < vector_Size(mounts); i++)
-		if (strcmp(((__VirtualFilesystem_Mount *)vector_At(mounts, i))->where, m.where) == 0) {
-			delete[] m.where;
+		if ((*(__VirtualFilesystem_Mount **)vector_At(mounts, i))->where == m->where) {
+			delete m;
+			INTERRUPT_RESTORE;
 			return -EEXIST;
 		}
 
 	vector_Push(mounts, &m);
+	INTERRUPT_RESTORE;
 	return 0;
 }
 
@@ -52,27 +49,97 @@ int VirtualFilesystem::Umount(const char *where) {
 	return -ENOSYS;
 }
 
-Filesystem *VirtualFilesystem::FilesystemOfPath(const char *path) {
-	Filesystem *fs     = root;
+Filesystem *VirtualFilesystem::FilesystemOfPath(const char *path, const char **fspath) {
+	Filesystem *fs     = root.fs;
 	size_t      maxlen = 1;
 
+	INTERRUPT_DISABLE;
 	for (int i = 0; i < vector_Size(mounts); i++) {
 		__VirtualFilesystem_Mount *m = (__VirtualFilesystem_Mount *)vector_At(mounts, i);
-		if (strncmp(m->where, path, m->len) == 0 && m->len > maxlen) {
-			maxlen = m->len;
+		if (strncmp(m->where.C(), path, m->where.Length()) == 0 && m->where.Length() > maxlen) {
+			maxlen = m->where.Length();
 			fs     = m->fs;
 		}
+	}
+	INTERRUPT_RESTORE;
+
+	if (fspath) {
+		if (fs == root.fs)
+			(*fspath) = path; // path==fspath on root filesystem
+		else
+			(*fspath) = path + maxlen; // fspath should begin with '/'
 	}
 
 	return fs;
 }
 
 
-struct __VirtualFilesystem_OpenFile {
-	Filesystem          *fs;       // Underlying filesystem.
-	Filesystem::OpenFile userfile; // Underlying OpenFile struct, passed to the underlying filesystem.
-};
+VirtualFilesystem::__VirtualFilesystem_OpenFile *VirtualFilesystem::__CreateOpenFile(const char *path) {
+	__VirtualFilesystem_OpenFile *file = new __VirtualFilesystem_OpenFile;
+	const char                   *fspath;
+	file->fs     = FilesystemOfPath(path, &fspath);
+	file->fspath = fspath;
 
+	return file;
+}
+
+int VirtualFilesystem::Open(const char *path, VirtualFilesystem::OpenFile *file) {
+	__VirtualFilesystem_OpenFile *vfsfile = __CreateOpenFile(path);
+
+	if (!(vfsfile->fs->Capabilities() | Capability_NoOpen)) {
+		int userret = vfsfile->fs->Open(vfsfile->fspath.C(), &vfsfile->userfile);
+		if (userret < 0) {
+			delete vfsfile;
+			return userret;
+		}
+	}
+
+	file->handle = (uint64_t)vfsfile;
+	return 0;
+}
+int VirtualFilesystem::Close(const char *path, VirtualFilesystem::OpenFile *file) {
+	__VirtualFilesystem_OpenFile *vfsfile = (__VirtualFilesystem_OpenFile *)file->handle;
+
+	int userret = 0;
+	if (!(vfsfile->fs->Capabilities() | Capability_NoOpen))
+		userret = vfsfile->fs->Close(vfsfile->fspath.C(), &vfsfile->userfile);
+	delete vfsfile;
+	return userret;
+}
+
+
+int VirtualFilesystem::Opendir(const char *path, VirtualFilesystem::OpenFile *file) {
+	__VirtualFilesystem_OpenFile *vfsfile = __CreateOpenFile(path);
+
+	if (!(vfsfile->fs->Capabilities() | Capability_NoOpen)) {
+		int userret = vfsfile->fs->Opendir(vfsfile->fspath.C(), &vfsfile->userfile);
+		if (userret < 0) {
+			delete vfsfile;
+			return userret;
+		}
+	}
+
+	file->handle = (uint64_t)vfsfile;
+	return 0;
+}
+int VirtualFilesystem::Readdir(const char *path, void *user, Readdir_Callback callback, OpenFile *file) {
+	__VirtualFilesystem_OpenFile *vfsfile = (__VirtualFilesystem_OpenFile *)file->handle;
+	return vfsfile->fs->Readdir(vfsfile->fspath.C(), user, callback, &vfsfile->userfile);
+}
+int VirtualFilesystem::Closedir(const char *path, OpenFile *file) {
+	__VirtualFilesystem_OpenFile *vfsfile = (__VirtualFilesystem_OpenFile *)file->handle;
+
+	int userret = 0;
+	if (!(vfsfile->fs->Capabilities() | Capability_NoOpen))
+		userret = vfsfile->fs->Closedir(vfsfile->fspath.C(), &vfsfile->userfile);
+	delete vfsfile;
+	return userret;
+}
+
+uintptr_t VirtualFilesystem::Ioctl(const char *path, uintptr_t cmd, void *arg, OpenFile *file) {
+	__VirtualFilesystem_OpenFile *vfsfile = (__VirtualFilesystem_OpenFile *)file->handle;
+	return vfsfile->fs->Ioctl(vfsfile->fspath.C(), cmd, arg, &vfsfile->userfile);
+}
 
 } // namespace filesystem
 } // namespace helos
--- driver/filesystem/vfs/vfs.hpp
+++ driver/filesystem/vfs/vfs.hpp
@@ -3,6 +3,7 @@
 #include "../../../cppruntime/runtime.hpp"
 #include "../filesystem.hpp"
 #include "../../../util/vector.h"
+#include "../../../cppruntime/string.hpp"
 
 namespace helos {
 namespace filesystem {
@@ -14,12 +15,15 @@ public:
 	VirtualFilesystem();
 	~VirtualFilesystem();
 
+	virtual const char *GetFilesystemType() override { return "vfs"; }
+	virtual Capability  Capabilities() override { return Capability(0); }
+
 public:
 	// Mount mounts the filesystem at the given path.
 	//
 	// Negative errno is returned on error.
 	//   -EEXIST  If the path already have a mount.
-	int Mount(const char *where, Filesystem *fs);
+	int Mount(const char *where, const char *source, Filesystem *fs);
 
 	// Umount unmounts a filesystem from the given path.
 	// The Filesystem* object is deleted.
@@ -28,18 +32,55 @@ public:
 	//   -ENOENT  If the path does not have a mount.
 	int Umount(const char *where);
 
+	// Callback for ListMount
+	typedef void (*ListMount_Callback)(void *user, const char *where, const char *source, Filesystem *fs);
+	// List all the mounts in the system.
+	int ListMount(void *user, ListMount_Callback callback);
+
 	// FilesystemOfPath returns the Filesystem on which
 	// the path is to be found.
+	//
+	// If fspath is not NULL, (*fspath) is set to the path
+	// of the file inside the underlying filesystem, pointing
+	// into the path string.
 	//
 	// It takes the last part into account. E.g., it returns
 	// the root filesystem if called with "/", and the device
 	// filesystem if called with "/dev" or "/dev/".
-	Filesystem *FilesystemOfPath(const char *path);
+	Filesystem *FilesystemOfPath(const char *path, const char **fspath);
+
+public:
+	virtual int Open(const char *path, OpenFile *file) override;
+	virtual int Close(const char *path, OpenFile *file) override;
+
+	virtual int Opendir(const char *path, OpenFile *file) override;
+	virtual int Readdir(const char *path, void *user, Readdir_Callback callback, OpenFile *file) override;
+	virtual int Closedir(const char *path, OpenFile *file) override;
+
+	virtual uintptr_t Ioctl(const char *path, uintptr_t cmd, void *arg, OpenFile *file) override;
 
 private:
-	Filesystem    *root;
-	vector_Vector *mounts;
+	struct __VirtualFilesystem_Mount {
+		runtime::String where, source;
+		Filesystem     *fs;
+	};
+	struct __VirtualFilesystem_OpenFile {
+		Filesystem          *fs;       // Underlying filesystem.
+		runtime::String      fspath;   // Filesystem path. Allocated.
+		Filesystem::OpenFile userfile; // Underlying OpenFile struct, passed to the underlying filesystem.
+	};
+
+	// Allocates a new __VFS_OpenFile structure.
+	// It does not call OpenXXX() of the underlying FS.
+	__VirtualFilesystem_OpenFile *__CreateOpenFile(const char *path);
+
+private:
+	__VirtualFilesystem_Mount root;
+	vector_Vector            *mounts;
 };
+
+// The global virtual filesystem.
+extern VirtualFilesystem *VFS;
 
 
 } // namespace filesystem
